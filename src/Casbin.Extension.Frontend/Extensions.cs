0ff0506652b9fff488928ba9895aafd3a4662cfe
Valeriy.Esipenko <elmankross@gmail.com>
2020-12-19 18:32:40 +0300

    Init abstractions and implementation from https://github.com/casbin/node-casbin/blob/master/src/frontend.ts
    relates to casbin/Casbin.NET#105

---
+++ src/Casbin.Extension.Frontend/Extensions.cs
@@ -0,0 +1,78 @@
+﻿using NetCasbin.Abstractions;
+using NetCasbin.Model;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Casbin.Extension.Frontend
+{
+    using Providers;
+
+    public static class Extensions
+    {
+        private static readonly string[] _policyKeys = new[] { "r", "p", "g", "e", "m" };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="enforcer"></param>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public static string CasbinJsGetPermissionForUser(this IEnforcer enforcer, string user)
+        {
+            var model = enforcer.GetModel().ToKeyDictionary();
+            var sb = new StringBuilder();
+            sb.AppendLine("[request_definition]")
+              .AppendLine(model["r"])
+              .AppendLine("[policy_definition]")
+              .AppendLine(model["p"]);
+            if (model["g"] != null)
+            {
+                sb.AppendLine("[role_definition]")
+                  .AppendLine(model["g"]);
+            }
+            sb.AppendLine("[policy_effect]")
+              .AppendLine(model["e"])
+              .AppendLine("[matchers]")
+              .AppendLine(model["m"]);
+
+            var jb = new JsonBuilder()
+                .WriteOpenObject()
+                .WriteKey("m")
+                .WriteValue(sb.ToString());
+            sb.Clear();
+
+            var policy = enforcer.GetPolicy();
+            jb.WriteKey("p");
+            foreach(var p in policy)
+            {
+                jb.WriteValue(p);
+            }
+
+            jb.WriteCloseObject();
+            return jb.ToString();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        private static IReadOnlyDictionary<string, string> ToKeyDictionary(this Model model)
+        {
+            var dict = model.Model;
+            var result = new Dictionary<string, string>(dict.Keys.Count);
+            foreach (var key in _policyKeys)
+            {
+                string value = null;
+                if (dict.ContainsKey(key) && dict[key].ContainsKey(key))
+                {
+                    var assertion = dict[key][key];
+                    value = assertion.Value?.Replace("_", ".");
+                }
+                result.Add(key, value);
+            }
+            return result;
+        }
+    }
+}
---
+++ src/Casbin.Extension.Frontend/Providers/JsonBuilder.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Casbin.Extension.Frontend.Providers
+{
+    /// <summary>
+    /// Simple json builder.
+    /// </summary>
+    /// <remarks>Own implementation 'cauze System.Text.Json supports .Net >= 4.6</remarks>
+    internal class JsonBuilder
+    {
+        private readonly StringBuilder _buffer;
+        private bool _thereIsKey;
+
+        internal JsonBuilder()
+        {
+            _buffer = new StringBuilder();
+            _thereIsKey = false;
+        }
+
+        public JsonBuilder WriteOpenObject()
+        {
+            _buffer.Append("{");
+            return this;
+        }
+
+        public JsonBuilder WriteCloseObject()
+        {
+            _buffer.Append("}");
+            _thereIsKey = false;
+            return this;
+        }
+
+        public JsonBuilder WriteKey(string key)
+        {
+            if (_thereIsKey)
+            {
+                _buffer.Append(",");
+            }
+
+            _buffer.Append("\"").Append(key).Append("\":");
+            _thereIsKey = true;
+            return this;
+        }
+
+        public JsonBuilder WriteValue(string value)
+        {
+            _buffer.Append("\"").Append(value).Append("\"");
+            return this;
+        }
+
+        public JsonBuilder WriteValue(ICollection<string> value)
+        {
+            _buffer.Append("[");
+            var isValue = false;
+            foreach (var v in value)
+            {
+                if (isValue)
+                {
+                    _buffer.Append(",");
+                }
+                WriteValue(v);
+                isValue = true;
+            }
+            _buffer.Append("]");
+            return this;
+        }
+
+
+        public override string ToString()
+        {
+            return _buffer.ToString();
+        }
+    }
+}
